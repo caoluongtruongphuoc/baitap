62e353101d0197eb6498c42d94299cc5b9897f54
caoluongtruongphuoc <caophong8991@gmaill.com>
2021-05-31 13:55:02 +0700

    update plugin

--- app/code/AHT/Salesagent/Plugin/SalesagentFirstname.php
+++ app/code/AHT/Salesagent/Plugin/SalesagentFirstname.php
@@ -5,7 +5,7 @@ class SalesagentFirstname
 {
     public function afterGetFirstname(\Magento\Customer\Model\Data\Customer $subject, $result) 
     {
-        if ($subject->getCustomAttribute('is_sales_agent')->getValue() == 1) {
+        if ($subject->getCustomAttribute('is_sales_agent') && $subject->getCustomAttribute('is_sales_agent')->getValue() == 1) {
             $result = "Sales Agent: " . $result;
         }
 
