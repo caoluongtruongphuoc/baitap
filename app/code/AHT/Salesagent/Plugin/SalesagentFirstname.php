b216742d0efc7e3589990698a9ae16d1aa8630d8
caoluongtruongphuoc <caophong8991@gmaill.com>
2021-05-31 11:47:02 +0700

    bai2(continue)

---
+++ app/code/AHT/Salesagent/Plugin/SalesagentFirstname.php
@@ -0,0 +1,14 @@
+<?php
+namespace AHT\Salesagent\Plugin;
+
+class SalesagentFirstname
+{
+    public function afterGetFirstname(\Magento\Customer\Model\Data\Customer $subject, $result) 
+    {
+        if ($subject->getCustomAttribute('is_sales_agent')->getValue() == 1) {
+            $result = "Sales Agent: " . $result;
+        }
+
+        return $result;
+    }
+}
