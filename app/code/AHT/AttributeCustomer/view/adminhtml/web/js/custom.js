343797f566c18c8656b0db8ea52ee18f0579df4c
caoluongtruongphuoc <caophong8991@gmaill.com>
2021-06-02 09:44:47 +0700

    bai3 update

--- app/code/AHT/AttributeCustomer/view/adminhtml/web/js/custom.js
+++ app/code/AHT/AttributeCustomer/view/adminhtml/web/js/custom.js
@@ -13,7 +13,10 @@ require(['jquery','domReady!'], function ($){
     var selector = '[name="customer[company_type]"]';
     waitForEl(selector, function() {
         // work the magic
-        $('[data-index="organization_name"]').hide();
+        if ($('[name="customer[company_type]"]').val() == 4) 
+            $('[data-index="organization_name"]').show();
+        else $('[data-index="organization_name"]').hide();
+       
         $('[name="customer[company_type]"]').change(function() {
             var data = $(this).val();
             if(data == 4){
